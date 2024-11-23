e4cfd27f4f23a5df21cae89215b457f451e824ef
seanderham1 <157894723+seanderham1@users.noreply.github.com>
2024-11-23 22:26:21 +0700

    Adding profile photo

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -28,7 +28,7 @@ export const RESUME_DATA = {
     "Detail-oriented Full Stack Engineer dedicated to building high-quality products.",
   summary:
     "As a Full Stack Engineer, I specialize in taking products from concept to launch. I excel in leading teams and creating environments where individuals perform at their best. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience working remotely with companies worldwide.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
+  avatarUrl: "https://raw.githubusercontent.com/seanderham1/seanwebsite/refs/heads/rollback-branch/src/assets/images/profile-cv.jpg?token=GHSAT0AAAAAAC2IBDH4LP3S44MCRX3IQ2NSZ2B6QYA",
   personalWebsiteUrl: "https://jarocki.me",
   contact: {
     email: "bartosz.jarocki@hey.com",
